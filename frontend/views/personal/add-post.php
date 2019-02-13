e358c9472b217e4f5dc1e9a3eeea5bf21c6af7ac
Nick <pyatakn@gmail.com>
2019-02-13 17:04:03 +0300

    upd

--- frontend/views/personal/add-post.php
+++ frontend/views/personal/add-post.php
@@ -1,10 +1,12 @@
-
+<?php
+use yii\helpers\Url;
+?>
 
 <section class="mixer_section">
 	<div class="contain">
 		<div class="section_top">
 			<h3 class="section_name">Создай новый трек</h3>
-			<a href="#" class="regulations_button">Правила пользования dj микшером</a>
+			<a href="<?=Url::toRoute(['site/faq']);?>" class="regulations_button">Правила пользования dj микшером</a>
 		</div>
 
 		<?=$this->render('_mixer');?>
