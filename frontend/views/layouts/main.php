7c2505c80fcf206c1e1ad22e371b9b377f543013
webcustom <webcustom1@gmail.com>
2019-02-14 16:33:07 +0300

    edits

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,7 +16,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="icon" href="img/favicon/favicon.ico" type="image/x-icon">
-    <link rel="apple-touch-icon" href="img/favicon/apple-touch-icon-180x180.png">
+    <link rel="apple-touch-icon" href="img/favicon/tnt-144.png">
     <!-- <link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
     <link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png"> -->
 
