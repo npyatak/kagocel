dde8204a0aed7afe50f116c93da14d112a18ce54
webcustom <webcustom1@gmail.com>
2019-02-24 12:22:21 +0300

    edits

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -5,31 +5,24 @@ $this->title = 'О продукте';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<section class="section_main gradient_bg after_header">
-	<div class="decorate_letter">
-		<img src="/img/letter_o.png" alt="img">
-	</div>
+
+
+<section class="section_main dark_bg bottom after_header min_h">
 
 	<div class="contain pt_small">
 		
 
-		<div class="main_wrap_blocks two_packt and_button">
-			<div class="wrap_big_img"><img class="big_img" src="/img/packt.png" alt="img"></div>
-
-			<div class="wrap_big_img"><img class="big_img" src="/img/packt2.png" alt="img"></div>
-
+		<div class="two_packt and_button">
+			<img class="" src="/img/two_packs.png" alt="img">
 		</div>
 
-	</div>
-</section>
-
-<section class="about_product pb_big">
-	<div class="contain">
-
-		<div class="section_top">
-			<h3 class="section_name">выб<i>о</i>р специалистов</h3>
-			<p class="section_desc">Кагоцел - противовирусное средство широкого спектра действия, которое применяется для профилактики и лечения простуды и гриппа у взрослых и детей с 3-х лет.</p>
+		<div class="about_product pb_big">
+			<div class="section_top">
+				<h3 class="section_name">выб<i>о</i>р специалистов</h3>
+				<p class="section_desc">Кагоцел - противовирусное средство широкого спектра действия, которое применяется для профилактики и лечения простуды и гриппа у взрослых и детей с 3-х лет.</p>
+			</div>
+			<a href="https://www.kagocel.ru/" target="_blank" class="button_1 point"><span>Подробнее о продукте</span></a>
 		</div>
-		<a href="https://www.kagocel.ru/" target="_blank" class="button_bg black_gray"><span>Подробнее о продукте</span></a>
+
 	</div>
 </section>
