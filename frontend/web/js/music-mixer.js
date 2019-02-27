6edb3982a2f6589f77f4ccb095cc7a6d6b3d2e4f
Азат Аскеров <askazat97@gmail.com>
2019-02-27 11:12:33 +0600

    Post['audio']

--- frontend/web/js/music-mixer.js
+++ frontend/web/js/music-mixer.js
@@ -488,7 +488,7 @@ function startRecording() {
       console.log("SENDING FILE");
 
       var formData = new FormData();
-      formData.append("audio", ourFile);
+      formData.append("Post['audio']", ourFile);
 
       var xhr = new XMLHttpRequest();
       xhr.open("post", "/personal/add-post");
