f4bd02a458391f450a6b8cd3a00f36755bc328c9
Azat Askerov <askazat97@gmail.com>
2019-03-05 09:59:16 +0600

    metrics

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -89,6 +89,23 @@ AppAsset::register($this);
                 }
             };
             </script>
+            <script>
+                window['_fs_debug'] = false;
+                window['_fs_host'] = 'fullstory.com';
+                window['_fs_org'] = 'JRMJ7';
+                window['_fs_namespace'] = 'FS';
+                (function(m,n,e,t,l,o,g,y){
+                if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
+                g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
+                o=n.createElement(t);o.async=1;o.src='https://'+_fs_host+'/s/fs.js';
+                y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
+                g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
+                g.shutdown=function(){g("rec",!1)};g.restart=function(){g("rec",!0)};
+                g.consent=function(a){g("consent",!arguments.length||a)};
+                g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
+                g.clearUserCookie=function(){};
+                })(window,document,window['_fs_namespace'],'script','user');
+            </script>
             <noscript><img src="//www.tns-counter.ru/V13a****tnt_ru/ru/CP1251/tmsec=tnt_online/" width="1" height="1" alt="" /></noscript>
             <noscript><div><img src="https://mc.yandex.ru/watch/32937699" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
     <?php //endif;?>
@@ -96,6 +113,11 @@ AppAsset::register($this);
     <?php $this->head() ?>
 </head>
 <body>
+<img 
+    src="http://r.mail.ru/r/4166?btype=show&gpmddealid=38500753&puid1=27&puid2=1&puid3=28&%random%" 
+    style="width:0;height:0;position:absolute;visibility:hidden;" 
+    alt=""
+/>
 <?php $this->beginBody() ?>
     <div class="wrapper">
 
