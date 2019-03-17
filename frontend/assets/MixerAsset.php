bcd6d5cf4e45e9710504b8b3522199971d850065
Azat Askerov <askazat97@gmail.com>
2019-03-18 04:32:09 +0600

    fix test

--- frontend/assets/MixerAsset.php
+++ frontend/assets/MixerAsset.php
@@ -9,7 +9,7 @@ class MixerAsset extends AssetBundle
     public $baseUrl = '@web';
     
     public $js = [
-		'js/AudioContextMonkeyPatch.js',
+		// 'js/AudioContextMonkeyPatch.js',
 		'mixer/js/WebMIDIAPI.js',
         'js/track.js',
 		'js/controls.js',
