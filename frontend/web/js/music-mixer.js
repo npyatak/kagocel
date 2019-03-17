973ad98e0fe628ca7e61707187c47a2b1e27c8d5
Azat Askerov <askazat97@gmail.com>
2019-03-18 05:47:13 +0600

    fix test

--- frontend/web/js/music-mixer.js
+++ frontend/web/js/music-mixer.js
@@ -536,41 +536,6 @@ function startRecording() {
     var ourFile = new File([blob], "audio.mp3", { type: "audio/mp3" });
     console.log(ourFile);
 
-    audioCtxS.first.audioCtx.suspend();
-    audioCtxS.second.audioCtx.suspend();
-
-    cancelAnimationFrame(audioCtxS.first.analyserAnimation);
-    cancelAnimationFrame(audioCtxS.second.analyserAnimation);
-
-    resetMixer();
-
-    setTimeout(function() {
-      audioCtxS.first.loudLines.map(function(line) {
-        line.css(
-          "height",
-          "0%"
-        );
-      });
-      audioCtxS.second.loudLines.map(function(line) {
-        line.css(
-          "height",
-          "0%"
-        );
-      });
-    }, 2000);
-
-    firstTrackPlayButton.closest(".play_stop").removeClass("active");
-    secondTrackPlayButton.closest(".play_stop").removeClass("active");
-
-    setTimeout(function() {
-      $("#first-handle").removeClass("active");
-      $("#second-handle").removeClass("active");
-    }, 100);
-
-    $(".plate").removeClass("spinning");
-
-    stopTimer();
-
     $("#mixer__done-message").css({ display: "block" });
     $(".mixer_playlist").css({ display: "none" });
     $("#mixer").css({ display: "none" });
@@ -584,6 +549,8 @@ function startRecording() {
     //     )
     //   );
     // }, 500);
+
+    $("#mixer__record-button .text").html("Начать запись");
 
     $("#resultSend").on("click", function() {
       console.log("SENDING FILE");
@@ -640,6 +607,43 @@ function startRecording() {
 }
 
 function stopRecording() {
+  audioCtxS.first.audioCtx.suspend();
+  audioCtxS.second.audioCtx.suspend();
+
+  cancelAnimationFrame(audioCtxS.first.analyserAnimation);
+  cancelAnimationFrame(audioCtxS.second.analyserAnimation);
+
+  resetMixer();
+
+  setTimeout(function() {
+    audioCtxS.first.loudLines.map(function(line) {
+      line.css(
+        "height",
+        "0%"
+      );
+    });
+    audioCtxS.second.loudLines.map(function(line) {
+      line.css(
+        "height",
+        "0%"
+      );
+    });
+  }, 2000);
+
+  firstTrackPlayButton.closest(".play_stop").removeClass("active");
+  secondTrackPlayButton.closest(".play_stop").removeClass("active");
+
+  setTimeout(function() {
+    $("#first-handle").removeClass("active");
+    $("#second-handle").removeClass("active");
+  }, 100);
+
+  $(".plate").removeClass("spinning");
+
+  stopTimer();
+
+  $("#mixer__record-button .text").html("Обрабатывается");
+
   mediaRecorder.finishRecording();
 }
 
@@ -940,139 +944,139 @@ function Audio(audioCtxLink, musicLink, musicInfo) {
   };
 }
 
-// function Sample(url) {
-//   this.url = url;
-//   this.request = null;
+function Sample(url) {
+  this.url = url;
+  this.request = null;
 
-//   this.getAudioBuffer = function(callback) {
-//     console.log("we are here");
-//     this.request = new XMLHttpRequest();
-//     var request = this.request;
-//     this.request.open("GET", this.url, true);
-//     this.request.responseType = "arraybuffer";
+  this.getAudioBuffer = function(callback) {
+    console.log("we are here");
+    this.request = new XMLHttpRequest();
+    var request = this.request;
+    this.request.open("GET", this.url, true);
+    this.request.responseType = "arraybuffer";
 
-//     this.request.onload = function() {
-//       callback(request.response);
-//     }
+    this.request.onload = function() {
+      callback(request.response);
+    }
 
-//     this.request.send();
-//   }
-// }
+    this.request.send();
+  }
+}
 
-// function SampleAudioCtx() {
-//   this.audioCtx = null;
-//   this.stream = null;
-//   this.gain = null;
+function SampleAudioCtx() {
+  this.audioCtx = null;
+  this.stream = null;
+  this.gain = null;
 
-//   this.init = function() {
-//     this.audioCtx = new AudioContext();
-//     this.stream = this.audioCtx.createMediaStreamDestination();
-//     this.gain = this.audioCtx.createGain();
-//     this.gain.gain.value = 0.5;
+  this.init = function() {
+    this.audioCtx = new AudioContext();
+    this.stream = this.audioCtx.createMediaStreamDestination();
+    this.gain = this.audioCtx.createGain();
+    this.gain.gain.value = 0.5;
     
-//     this.gain.connect(this.audioCtx.destination);
-//     console.log(this.audioCtx, "AUDIO CTX");
-//   }
+    this.gain.connect(this.audioCtx.destination);
+    console.log(this.audioCtx, "AUDIO CTX");
+  }
 
-//   this.changeVolume = function(vol) {
-//     this.gain.gain.value = vol;
-//   }
+  this.changeVolume = function(vol) {
+    this.gain.gain.value = vol;
+  }
 
-//   this.playSample = function(link) {
-//     var source = this.audioCtx.createBufferSource();
-//     var sample = new Sample(link);
-//     sample.getAudioBuffer(function(data) {
-//       console.log('WE ARE HERE');
-//       this.audioCtx.decodeAudioData(data, function(buffer) {
-//         source.buffer = buffer;
-//         source.connect(this.gain);
-//         source.loop = false;
-//         source.start(0);
-//       }.bind(this))
-//     }.bind(this));
-//   }
-// }
+  this.playSample = function(link) {
+    var source = this.audioCtx.createBufferSource();
+    var sample = new Sample(link);
+    sample.getAudioBuffer(function(data) {
+      console.log('WE ARE HERE');
+      this.audioCtx.decodeAudioData(data, function(buffer) {
+        source.buffer = buffer;
+        source.connect(this.gain);
+        source.loop = false;
+        source.start(0);
+      }.bind(this))
+    }.bind(this));
+  }
+}
 
-// window.sampleAudioCtx = new SampleAudioCtx();
-// sampleAudioCtx.init();
+window.sampleAudioCtx = new SampleAudioCtx();
+sampleAudioCtx.init();
 
-// $(".mixer__additional-track-player").on("click", function(e) {
-//   var audioUrl = $(this).data("url");
-//   console.log(audioUrl);
-//   sampleAudioCtx.playSample(audioUrl);
-// })
+$(".mixer__additional-track-player").on("click", function(e) {
+  var audioUrl = $(this).data("url");
+  console.log(audioUrl);
+  sampleAudioCtx.playSample(audioUrl);
+})
 
-// function Preplay(url) {
-//   this.url = url;
-//   this.request = null;
+function Preplay(url) {
+  this.url = url;
+  this.request = null;
 
-//   this.getAudioBuffer = function(callback) {
-//     console.log("we are here");
-//     this.request = new XMLHttpRequest();
-//     var request = this.request;
-//     this.request.open("GET", this.url, true);
-//     this.request.responseType = "arraybuffer";
+  this.getAudioBuffer = function(callback) {
+    console.log("we are here");
+    this.request = new XMLHttpRequest();
+    var request = this.request;
+    this.request.open("GET", this.url, true);
+    this.request.responseType = "arraybuffer";
 
-//     this.request.onload = function() {
-//       callback(request.response);
-//     }
+    this.request.onload = function() {
+      callback(request.response);
+    }
 
-//     this.request.send();
-//   }
-// }
+    this.request.send();
+  }
+}
 
-// function PreplayAudioCtx() {
-//   this.audioCtx = null;
-//   this.gain = null;
-//   this.source = null;
+function PreplayAudioCtx() {
+  this.audioCtx = null;
+  this.gain = null;
+  this.source = null;
 
-//   this.init = function() {
-//     this.audioCtx = new AudioContext();
-//     this.gain = this.audioCtx.createGain();
-//     this.gain.gain.value = 0.5;
+  this.init = function() {
+    this.audioCtx = new AudioContext();
+    this.gain = this.audioCtx.createGain();
+    this.gain.gain.value = 0.5;
 
-//     this.gain.connect(this.audioCtx.destination);
-//     console.log(this.audioCtx, "AUDIO CTX");
-//   }
+    this.gain.connect(this.audioCtx.destination);
+    console.log(this.audioCtx, "AUDIO CTX");
+  }
 
-//   this.playSample = function(link) {
-//     var source = this.audioCtx.createBufferSource();
-//     var sample = new Preplay(link);
-//     sample.getAudioBuffer(function(data) {
-//       console.log('WE ARE HERE');
-//       this.audioCtx.decodeAudioData(data, function(buffer) {
-//         source.buffer = buffer;
-//         source.connect(this.gain);
-//         source.loop = false;
-//         source.start(0);
+  this.playSample = function(link) {
+    var source = this.audioCtx.createBufferSource();
+    var sample = new Preplay(link);
+    sample.getAudioBuffer(function(data) {
+      console.log('WE ARE HERE');
+      this.audioCtx.decodeAudioData(data, function(buffer) {
+        source.buffer = buffer;
+        source.connect(this.gain);
+        source.loop = false;
+        source.start(0);
 
-//         this.source = source;
-//       }.bind(this))
-//     }.bind(this));
-//   }
+        this.source = source;
+      }.bind(this))
+    }.bind(this));
+  }
 
-//   this.stop = function() {
-//     if (this.source) {
-//       this.source.stop();
-//     }
-//   }
-// }
+  this.stop = function() {
+    if (this.source) {
+      this.source.stop();
+    }
+  }
+}
 
-// window.preplayAudioCtx = new PreplayAudioCtx();
-// preplayAudioCtx.init();
+window.preplayAudioCtx = new PreplayAudioCtx();
+preplayAudioCtx.init();
 
-// $(".preplay_sound").on("click", function(e) {
-//   if ($(e.target).html() === "Прослушать") {
-//     preplayAudioCtx.stop();
-//     $(".preplay_sound").html("Прослушать");
-//     preplayAudioCtx.playSample(($(e.target).closest(".play_li").data("sound_url")));
-//     $(e.target).html("Остановить");
-//   } else {
-//     preplayAudioCtx.stop();
-//     $(".preplay_sound").html("Прослушать");
-//   }
+$(".preplay_sound").on("click", function(e) {
+  if ($(e.target).html() === "Прослушать") {
+    preplayAudioCtx.stop();
+    $(".preplay_sound").html("Прослушать");
+    preplayAudioCtx.playSample(($(e.target).closest(".play_li").data("sound_url")));
+    $(e.target).html("Остановить");
+  } else {
+    preplayAudioCtx.stop();
+    $(".preplay_sound").html("Прослушать");
+  }
   
-// })
+})
 
 $("#mixer__listen-second").on("click", function() { $("#mixer__result-play-button").click(); })
 $("#mixer__reset-settings").on("click", function(e) { resetMixer(); e.preventDefault(); e.stopPropagation(); })
