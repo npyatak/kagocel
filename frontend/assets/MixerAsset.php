1ae2f2aa946f9718c4529316a326578c66a10f7a
Azat Askerov <askazat97@gmail.com>
2019-03-18 05:53:09 +0600

    fix test

--- frontend/assets/MixerAsset.php
+++ frontend/assets/MixerAsset.php
@@ -10,7 +10,7 @@ class MixerAsset extends AssetBundle
     
     public $js = [
 		// 'js/AudioContextMonkeyPatch.js',
-		'mixer/js/WebMIDIAPI.js',
+		// 'mixer/js/WebMIDIAPI.js',
         'js/track.js',
 		'js/controls.js',
     ];
